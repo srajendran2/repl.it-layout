57e19abb9251cb85cf7f0d2c90ee6c1b1375d74d
srajendran2 <>
2021-05-24 19:40:20 +0000

    new

--- index.js
+++ index.js
@@ -1,7 +1,67 @@
-// const keyword is used to define constants for the application
-// value cannot be modified later in the program / application
-const var1 = "constant";
-console.log(var1);
+// addition between two numbers
+let a1 = 10;
+let a2 = 20;
+let addResult = a1 + a2;
+console.log(addResult);
 
-//the below statement will error out because constant values cannot be changed
-//var1 = "constant1"; 
+// subtraction between two numbers
+let subtractResult = a1 - a2;
+console.log(subtractResult);
+
+// multiplication between two numbers
+let multiplyResult = a1 * a2;
+console.log(multiplyResult);
+
+// division between two numbers
+let divisionResult = a1 / a2;
+console.log(divisionResult);
+
+// to power of
+let powerResult = a2 ** a1;
+console.log(powerResult);
+
+// modulus remainder
+let modulusResult = a2 % a1;
+console.log(modulusResult);
+
+// pre assignment increment
+let preincrementResult = ++a1;
+console.log(preincrementResult);
+
+// post assignment increment
+let postincrementResult = a2++;
+console.log(postincrementResult);
+
+//order of operation can be determined using PEDMAS
+//P = parentheses
+//E = exponentiation
+//M = multiplication
+//D = division
+//A = addition
+//S = subtraction
+
+console.log(16 - 2 * 5 / 3 + 1);
+console.log(1 + 5 % 3);
+console.log(2 ** 2 ** 3 * 3);
+
+//compound assignment operator
+let x = 1;
+let y = 1;
+let z = 1;
+let w = 1;
+
+// x = x + 1
+x += 1;
+console.log(x);
+
+// y = y - 1
+y -= 1;
+console.log(y);
+
+// z = z * 1
+z *= 1;
+console.log(z);
+
+// w = w * 1
+w /= 1;
+console.log(w);
