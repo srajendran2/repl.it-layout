d164688b3a5c51eb3a42677f73c38af30a303ffb
srajendran2 <>
2021-05-24 16:29:53 +0000

    added string append

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ str.concat(addresult);
 console.log('Coding Rocks');
 console.log("Coding Rocks");
 console.log("testing");
+console.log('Hello, JavaScript.');
+console.log(2001);
+
+console.log("What","do","commas","do?"); // Append string using commas
+console.log("Does", "adding",      "space", "matter?"); // adding space doesn't affect result
+console.log('Launch' + 'Code'); //using + symbol to append
+console.log("LaunchCode was founded in", 2013); //Using comma with quotes to append  
 
