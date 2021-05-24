c066cd48583a811461c06b1ee7c8b8b6401abc8d
srajendran2 <>
2021-05-24 21:17:52 +0000

    new

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
-let input = require('readline-sync');
-let userName = input.question("Enter name ");
-console.log("Hello " + userName + "!" + " let's do some addition");
+// define variables
+const shuttleName = "Determination";
+let shuttleSpeed = 17500;
+let distanceToMars = 225000000;
+let distanceToMoon = 384000;
+let milesPerKilometer = 0.621;
 
-//* numbers calculation 
-// input is accepted as a string, so number conversion is required
-let num1 = input.question("Enter 1st number ");
-let num2 = input.question("Enter 2nd number ");
-let sum = Number(num1) + Number(num2);
-console.log("total sum is " + sum);
+// identify data type 
+let v1 = typeof(shuttleName);
+console.log(v1);
+
+v1 = typeof(shuttleSpeed);
+console.log(v1);
+
+v1 = typeof(distanceToMars);
+console.log(v1);
+
+v1 = typeof(distanceToMoon);
+console.log(v1);
+
+v1 = typeof(milesPerKilometer);
+console.log(v1);
+
+// calculate miles to mars
+let milesToMars = distanceToMars * milesPerKilometer;
+console.log(milesToMars);
+
+// calculate hours to mars
+let hoursToMars = milesToMars / shuttleSpeed;
+console.log(hoursToMars);
+
+// days to mars
+let daysToMars = hoursToMars / 24;
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars");
+
+// calculate miles to moon
+let milesToMoon = distanceToMoon * milesPerKilometer;
+console.log(milesToMoon);
+
+// calculate hours to moon
+let hoursToMoon = milesToMoon / shuttleSpeed;
+console.log(hoursToMoon);
+
+// days to moon
+let daysToMoon = hoursToMoon / 24;
+console.log(shuttleName + " will take " + daysToMoon + " days to reach Moon");
