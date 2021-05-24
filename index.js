3c8525ddc72de06e9a3488262a2f2a672e64e391
srajendran2 <>
2021-05-24 16:53:28 +0000

    removed append

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ console.log("testing");
 console.log('Hello, JavaScript.');
 console.log(2001);
 
-console.log("What","do","commas","do?"); // Append string using commas
-console.log("Does", "adding",      "space", "matter?"); // adding space doesn't affect result
-console.log('Launch' + 'Code'); //using + symbol to append
-console.log("LaunchCode was founded in", 2013); //Using comma with quotes to append
-
-// add tab
 
 console.log("testing\ttab\nand\nnewline");
 
