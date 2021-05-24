efc0378a275d7ede2dabd1777870a854bb61d9b9
srajendran2 <>
2021-05-24 16:49:37 +0000

    typeof

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-var str = "hello's world";
-console.log(str);
-var addresult = 3+4;
-str.concat(addresult);
-console.log('Coding Rocks');
-console.log("Coding Rocks");
-console.log("testing");
-console.log('Hello, JavaScript.');
-console.log(2001);
-
-console.log("What","do","commas","do?"); // Append string using commas
-console.log("Does", "adding",      "space", "matter?"); // adding space doesn't affect result
-console.log('Launch' + 'Code'); //using + symbol to append
-console.log("LaunchCode was founded in", 2013); //Using comma with quotes to append
-
-// add tab
-
-console.log("testing\ttab\nand\nnewline");
-
+var var1 = "hello's world";
+console.log(var1);
+//typeof keyword 
+console.log("type of 'var1' variable is "+ typeof var1);
+var var2 = 1000;
+console.log(var2);
+console.log("type of 'var2' variable is "+ typeof var2);
+var var3 = 1000.10;
+console.log(var3);
+console.log("type of 'var3' variable is "+ typeof var3);
