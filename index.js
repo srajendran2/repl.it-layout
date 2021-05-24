bd7f86ae0deef57afa32e03a2a71dc1caa608417
srajendran2 <>
2021-05-24 16:55:39 +0000

    new branch

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-// Append string using commas
-console.log("What","do","commas","do?"); 
-// adding space doesn't affect result
-console.log("Does", "adding",      "space", "matter?"); 
-//using + symbol to append
-console.log('Launch' + 'Code'); 
-//Using comma with quotes to append
-console.log("LaunchCode was founded in", 2013); 
-
+// \t adds a space & \n introduces newline
+console.log("testing\ttab\nand\nnewline");
