bd97511f10231d2db10002c55ad64dbb1c31a06c
srajendran2 <>
2021-05-24 17:11:19 +0000

    added type casting / conversion

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ console.log(var3);
 console.log("type of 'var3' variable is "+ typeof var3);
 var var4 = "1000.10";
 console.log(var4);
-console.log("type of 'var4' variable is "+ typeof var4); 
+console.log("type of 'var4' variable is "+ typeof var4);
+
+// Type conversion Number and String functions help convert inputs to respective data types
+
+// Number examples
+console.log(Number("123 "));
+console.log(typeof Number("123 "));
+console.log(Number(17));
+
+// Number NaN - Not A Number
+console.log(Number("123a"));
+
+// String examples
+console.log(String(17));
+console.log(typeof String(17));
+console.log(String("..."));
