b180748c3381484c95acd739a3934badf90be38b
srajendran2 <>
2021-05-24 16:25:45 +0000

    comment lines

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//Single line comment
+
 var str = "hello's world";
 console.log(str);
-var addresult = 3+4;
-str.concat(addresult);
-console.log('Coding Rocks');
-console.log("Coding Rocks");
 
+/* multi
+   line
+   comment
+*/
