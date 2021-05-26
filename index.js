70fed4dc6126eae408b7084e8d621d0c00bb84ae
srajendran2 <>
2021-05-26 16:19:22 +0000

    new

--- index.js
+++ index.js
@@ -1,76 +1,44 @@
-let engineIndicatorLight = "red blinking";
-let spaceSuitsOn = true;
-let shuttleCabinReady = true;
-let crewStatus = (spaceSuitsOn && shuttleCabinReady);
-let computerStatusCode = 200;
-let shuttleSpeed =15000;
-let commandOverride = true;
+// Syntax error - the console log bracket is not closed
+let day = Wednesday;
+console.log(day;
 
-let input = require('readline-sync');
-let fuelLevel = input.question("Enter fuel level: ");
-let engineTemperature = input.question("Enter Engine temperature: ");
-engineIndicatorLight = input.question("Enter Engine indicator light: ");
-commandOverride = input.question("Enter commandOverride: ");
+let name = Julie;
+console.log("Hello, name);
 
-if (engineIndicatorLight === "green") {
-   console.log("engines have started");
-} else if (engineIndicatorLight === "green blinking") {
-   console.log("engines are preparing to start");
-} else {
-   console.log("engines are off");
-};
+// Runtime error - the variable used in console log is not defined
+let firstName = "Jack";
+console.log(firstname);
 
-if (crewStatus = true){
-    console.log("Crew Ready");
-}
-else{
-    console.log("Crew Not Ready");
-};
+let name = Julie;  // Julie should be in quotes
+console.log("Hello", name);
 
-if (computerStatusCode === 200){
-    console.log("Please stand by. Computer is rebooting.");
-}else if(computerStatusCode === 400){
-    console.log("Success! Computer online.");
-}else{
-    console.log("ALERT: Computer offline!");
-};
+// Logical error 
+// considering work week is 5 days only, using 7 is considered incorrect in below use case
+let weeklyPay = 600;
+let dailyEarnings = weeklyPay / 7;
+console.log(dailyEarnings);
 
-if (shuttleSpeed > 17500){
-    console.log("ALERT: Escape velocity reached!");
-} else if (shuttleSpeed < 8000){
-    console.log("ALERT: Cannot maintain orbit!");  
-} else{
-    console.log("Stable speed");   
-};
+// types of error
+// SyntaxError - Occurs when trying to parse syntactically invalid code.
+    // console.log("hello";
 
-if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
-   console.log("all systems go");
-} else {
-   console.log("WARNING. Not ready");
-};
 
-if (!crewStatus || computerStatusCode !== 200 || !spaceSuitsOn) {
-   console.log("WARNING. Not ready");
-} else {
-   console.log("all systems go");
-};
+// ReferenceError	- Occurs when a non-existent variable is used/referenced.	
+    // let firstName = "Jack";
+    // console.log(firstname); 
 
-if (fuelLevel > 20000 && engineTemperature <= 2500){
-  console.log("Full tank. Engines good.");
-} else if (fuelLevel > 10000 && engineTemperature <= 2500){
-  console.log("Fuel level above 50%.  Engines good.");
-} else if (fuelLevel > 5000 && engineTemperature <= 2500){
-  console.log("Fuel level above 25%. Engines good.");   
-} else if (fuelLevel <= 5000 && engineTemperature > 2500){
-  console.log("Check fuel level. Engines running hot.");
-} else if (fuelLevel < 1000 || engineTemperature > 3500 || engineIndicatorLight === "red blinking"){
-  console.log("ENGINE FAILURE IMMINENT!");  
-} else {
-  console.log("Fuel and engine status pending...");  
-};
 
-if((fuelLevel > 20000 && engineIndicatorLight !== "red blinking") || commandOverride === true){
-  console.log("Cleared to launch!");  
-} else {
-  console.log("Launch scrubbed!");    
-}
+// TypeError - Occurs when trying to use a value in an invalid way.	
+   //1(); 
+
+
+// RangeError - Occurs when passing an invalid value to a function.	
+  //  let nums = Array(-1);
+
+
+// URIError - Occurs when improperly using a global URI-handling function. ('URI' = Uniform Resource Identifier)	
+  //  decodeURI('%');
+
+  
+// Error - The type from which all other errors are built. It can be used to generate programmer-triggered and programmer-defined errors.	
+  //  throw Error("Something bad happened!");
