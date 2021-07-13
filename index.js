1572bdb9bbf9e3e73510d46fb650fca311ce9846
srajendran2 <80505001+srajendran2@users.noreply.github.com>
2021-07-13 15:08:41 +0000

    init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Strings are objects and they have certain functions / methods // associated with it
 // length function
-let s1 = "This is a long string";
+let s1 = " This is a long string  ";
 console.log(s1.length);
 
 // lowercase function
@@ -17,3 +17,15 @@ console.log(s1.substr(0,3))
 
 //substring(start,end)
 console.log(s1.substring(1,3))
+
+//trim - removes trailing and leading spaces
+let t1=s1.trim();
+let l1=t1.length;
+
+console.log(t1,l1);
+
+//indexof returns the first occurrence position of substr 
+console.log(s1.indexOf("long"));
+
+//replace chars - copy the string and replace chars
+console.log(s1.replace("long", "short"));
