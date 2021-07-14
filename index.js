aad91f75984b3a9c1ab38105b12b512d82fc2067
srajendran2 <80505001+srajendran2@users.noreply.github.com>
2021-07-14 07:00:06 +0000

    init

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ All-where.
 – Sri Chinmoy`;
 
 console.log(poem);
+//
+
+let input=require('readline-sync')
+let pluralNoun = input.question("Enter pluralNoun ");
+let name1 = input.question("Enter name ");
+let verb = input.question("Enter verb ");
+let adjective = input.question("Enter adjective ");
+let color = input.question("Enter color ");
+
+console.log(`JavaScript provides a ${color} collection of tools — including ${adjective} syntax and ${pluralNoun} that allows ${name1} to ${verb} with strings.`)
