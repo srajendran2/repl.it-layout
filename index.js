cbfd287334f0da1565ea07571b0aca04cec9f800
srajendran2 <80505001+srajendran2@users.noreply.github.com>
2021-07-14 06:45:44 +0000

    init

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// way to translate between characters and integers
-// the most recent versions of the Unicode character encoding    // include emoji characters, such as 🌮.
-//--------------------------------------------------------------
-// ASCII maps a to 97, b to 98, and so on for lowercase letters, // with z mapping to 122. Uppercase letters map to the values 65  // through 90. The other integers between 0 and 127 represent    // symbols, punctuation, and other assorted odd characters. 
-//---------------------------------------------------------------
-//The string method charCodeAt takes an index and returns the    // ASCII code of the character at that index.
-let s1 = "This is ASCII encoding";
-console.log(s1.charCodeAt(5));
-console.log(s1.charCodeAt(0));
+// \n - new line
+// \t - tab
+console.log("A message\nbroken across lines,\n\tand indented");
 
-let n1=122;
-console.log(String.fromCharCode(n1));
+// \uXXXX - unicode
+console.log("The interrobang character, \u203d, combines ? and !");
+console.log("The character, \u00C9");
+
+// use escape character \ to include quotes in string
+console.log("\"The dog's favorite toy is a stuffed hedgehog,\" said Chris");
