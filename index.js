08b34b4ee86875422bcf257cbc17a33db1388e6f
srajendran2 <80505001+srajendran2@users.noreply.github.com>
2021-07-15 09:49:26 +0000

    init

--- index.js
+++ index.js
@@ -1,110 +1,35 @@
-// reference https://www.w3schools.com/jsref/jsref_obj_array.asp
-let a1 = ['a','b','c'];
-let a2 = ['d','e','f'];
-let a3 = ['g','h','i'];
-let a5 = [4,3,5];
+//two dimensional array
+let shuttleCrews = [
+   ['Robert Gibson', 'Mark Lee', 'Mae Jemison'],
+   ['Kent Rominger', 'Ellen Ochoa', 'Bernard Harris'],
+   ['Eilen Collins', 'Winston Scott',  'Catherin Coleman']
+];
 
-// concat - joins multiple arrays to create a new one
-let a4 = a1.concat(a2,a3);
-console.log(a4.length);
-console.log(a4[2],a4[5],a4[8]);
+console.log(shuttleCrews[0][2]);
+console.log(shuttleCrews[1][1]);
+console.log(shuttleCrews[2][1]);
 
-//includes - checks if the array contains it
-console.log(a4.includes('h'));
-console.log(a4.includes('j'));
 
-//indexof - the position of the value in array
-// not found returns -1
-console.log(a4.indexOf('h'));
-console.log(a4.indexOf('j'));
+let newCrew = ['Mark Polansky', 'Robert Curbeam', 'Joan Higginbotham'];
 
-// display entire array
-console.log(a4);
-console.log(a1);
-console.log(a2);
+// Add a new crew array to the end of shuttleCrews
+shuttleCrews.push(newCrew);
+console.log(shuttleCrews[3][2]);
 
-// reverse
-console.log(a4.reverse());
+// Reverse the order of the crew at index 1
+shuttleCrews[1].reverse();
+console.log(shuttleCrews[1]);
 
-//sort array value
-console.log(a5.sort());
+let school = [
+   ["science", "computer", "art"],
+   ["Jones", "Willoughby", "Rhodes"]
+];
 
-// pop - remove last element and returns it
-console.log(a5);
-a5.pop();
-console.log(a5);
+//display jones
+console.log(school[1][0]);
 
-//push - adds last element
-console.log(a5);
-a5.push(5);
-console.log(a5);
+// add a new row in two dimensional school array
+school.push(["dance"]);
+school[2][1]=("Holmes");
+console.log(school);
 
-//shift - removes the first element of array and returns it
-console.log(a5);
-a5.shift();
-console.log(a5);
-
-//unshift - adds 1 or more to the beginning of array and returns //length
-console.log(a5);
-a5.unshift(1,2,3);
-console.log(a5);
-
-//splice
-let arr = ['a', 'b', 'c', 'd', 'e', 'f'];
-arr.splice(2);    //Everything from index 2 and beyond is removed.
-console.log(arr);
-
-let arr1 = ['a', 'b', 'c', 'd', 'e', 'f'];
-//Start at index 2 and remove 3 total entries.
-arr1.splice(2,3);    
-console.log(arr1);
-
-//Start at index 1 and remove 1 entry.
-arr1.splice(1,1);    
-console.log(arr1);
-
-//Start at index 2, remove 0 entries, and add 'hello'.
-let arr2 = ['a', 'b', 'c', 'd', 'e', 'f'];
-arr2.splice(2,0,'hello'); 
-console.log(arr2);
-
-//Start at index 2, replace 3 entries with 'hello' and 9.
-let arr3 = ['a', 'b', 'c', 'd', 'e', 'f'];
-arr3.splice(2,3,'hello', 9);   
-console.log(arr3);
-
-// join - joins elements into string
-let arr4 = [1, 2, 3, 4];
-let words = ['hello', 'world', '!'];
-let newString = '';
-
-newString = arr4.join("+");
-console.log(newString);
-
-newString = words.join("");
-console.log(newString);
-
-newString = words.join("_");
-console.log(newString);
-
-// split - split string into array elements. split is determined // by delimeter
-let numbers = "1,2,3,4";
-let word = "Rutabaga";
-let phrase = "Bookkeeper of balloons."
-let arr5 = [];
-
-arr5 = numbers.split(',');  //split the string at each comma.
-console.log(arr5);
-
-arr5 = phrase.split(' ');   //split the string at each space.
-console.log(arr5);
-
-arr5 = word.split('');      //split the string at each character.
-console.log(arr5);
-
-// slice (start pos, end pos) - Extracts and creates new array
-let groceryBag = ['bananas', 'apples', 'edamame', 'chips', 'cucumbers', 'milk', 'cheese'];
-let selectedItems = [];
-
-selectedItems = groceryBag.slice(2, 5).sort();
-console.log(selectedItems);
