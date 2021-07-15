671dcee6d5afd4cdfafc006c965d9206e79d8833
srajendran2 <80505001+srajendran2@users.noreply.github.com>
2021-07-15 06:40:46 +0000

    init

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-// the back tick character ` allows variables to be included as part of output
+console.log("Wonderful".length);
+console.log('JavaScript'[8]);
+console.log("Strings are sequences of characters."[5]);
+console.log("Do spaces count?".length);
+// length of integer
+n1 = 20000;
+s1 = String(n1);
+console.log(s1.length);
+// chars after decimal
+n2 = 200.12;
+s2 = String(n2);
+console.log(s2.slice((s2.indexOf(".")+1),s2.length));
+if (s2.includes(".") === true){
+  console.log("It is decimal");
+} else {
+  console.log("it is integer");
+}
+// remove trailing leading spaces, convert to upper
+dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    "
+d1 = dna.trim().toUpperCase()
+dna = d1;
+console.log(dna);
+// Replace the sequence 'GCT' with 'AGG', and then print the      // altered strand.
+console.log(dna.replace("GCT","AGG"));
+// Look for the sequence 'CAT' with indexOf(). If found print,    // 'CAT found', otherwise print, 'CAT NOT found'.
+if (dna.indexOf("CAT") > 0){
+  console.log("CAT found");
+} else if (dna.indexOf("CAT") === -1){
+  console.log("CAT NOT found");
+}
+// Use slice() to print out the fifth set of 3 characters (called // a codon) from the DNA strand.
+console.log(dna.slice(16,19));
+// use template literal
+dnaLength = dna.length;
+console.log(`The DNA strand is ${dnaLength} characters long.`);
 
-let name = "Jack";
-let currentAge = 9;
-// normal output
-console.log("Next year, " + name + " will be " + (currentAge + 1) + ".");
 
-// back tick used output
-console.log(`Next year, ${name} will be ${currentAge + 1}.`);
-
-// back tick allows multi-line output without using \n
-let poem = `The mind chases happiness.
-The heart creates happiness.
-The soul is happiness
-And it spreads happiness
-All-where.
-
-– Sri Chinmoy`;
-
-console.log(poem);
-//
-
-let input=require('readline-sync')
-let pluralNoun = input.question("Enter pluralNoun ");
-let name1 = input.question("Enter name ");
-let verb = input.question("Enter verb ");
-let adjective = input.question("Enter adjective ");
-let color = input.question("Enter color ");
-
-console.log(`JavaScript provides a ${color} collection of tools — including ${adjective} syntax and ${pluralNoun} that allows ${name1} to ${verb} with strings.`)
