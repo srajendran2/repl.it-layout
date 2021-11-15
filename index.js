0c8dd3c041d3a9fe3336aec58da4cf68b6da6e5e
srajendran2 <80505001+srajendran2@users.noreply.github.com>
2021-11-15 15:47:32 +0000

    added lines

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var str = "hello's world";
 console.log(str);
 var addresult = 3+4;
-str.concat(addresult);
+console.log(str.concat(" " + addresult));
+console.log(addresult);
+console.log(str);
 console.log('Coding Rocks');
 console.log("Coding Rocks");
 console.log("testing");
-console.log('Hello, JavaScript.');
+console.log('Hello, "JavaScript."');
 console.log(2001);
 
 
