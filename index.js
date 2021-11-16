bbf0beffdc6e3645214b661224010e47177ea171
srajendran2 <80505001+srajendran2@users.noreply.github.com>
2021-11-16 20:48:14 +0000

    upd

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let day = Wednesday;
 console.log(day;
 
+// quotes missing
 let name = Julie;
 console.log("Hello, name);
 
@@ -29,14 +30,17 @@ console.log(dailyEarnings);
 
 
 // TypeError - Occurs when trying to use a value in an invalid way.	
+//The numeric value 1 is not a function, so trying to use it as one //results in TypeError: 1 is not a function.
    //1(); 
 
 
-// RangeError - Occurs when passing an invalid value to a function.	
+// RangeError - Occurs when passing an invalid value to a function.
+//The constructor function Array(n) creates an empty array of length //n. It is not possible to create an array with negative length, so //the code results in RangeError: Invalid array length.
   //  let nums = Array(-1);
 
 
-// URIError - Occurs when improperly using a global URI-handling function. ('URI' = Uniform Resource Identifier)	
+// URIError - Occurs when improperly using a global URI-handling //function. ('URI' = Uniform Resource Identifier)	
+//The % character is used to encode characters not otherwise allowed //in URIs, such as spaces (%20). If an invalid character encoding is //given, a URIError results.
   //  decodeURI('%');
 
   
